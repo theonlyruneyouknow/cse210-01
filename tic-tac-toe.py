7fcdfdf7599034c9cb4bb1fb87bef1cf7225085f
Rune Larsen <theonlyruneyouknow@gmail.com>
2022-01-20 09:39:10 -0800

    Removed un-needed old code

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -26,9 +26,6 @@ while winner != 0:
                 print ("X wins")
                 winner = 1
                 break
-            # if tac[0] == 'o' and tac[1] == 'o':
-            #     print ("O wins")
-            #     break
             if     (tac[0] == 'o' and tac[1] == 'o' and tac[2] == 'o') \
                 or (tac[3] == 'o' and tac[4] == 'o' and tac[5] == 'o') \
                 or (tac[6] == 'o' and tac[7] == 'o' and tac[8] == 'o') \
@@ -42,8 +39,6 @@ while winner != 0:
                 winner = 1
                 break
 
-            # if tac[0] == 'x':
-            #     print ("X wins")
             if whosturn == 'Px':
                 
                 if len(left) == 0:
@@ -58,19 +53,12 @@ while winner != 0:
                         raise ValueError
                     else:
                         x = x-1
-                        # if (tac[x] == 'x') or (tac[x] == 'o'):
-                        #     y = x = int(input("Try Again. X's turn to choose a square (1-9):"))
-                        #     print("tac[y] = ", tac[y])
-                        #     print("left[y] = ", left)
-                        # else:
                         tac.pop(x)
                         left.remove(y)
                         tac.insert(x,"x")
                         print ("input was " , y)
                         print("left[y] = ", left)
                         print("numbers left", len(left))
-                        # if len(left) == 0:
-                        #     print ("Cat's Game. No winner. Do you want to play again?")
                         whosturn = "Po"
             elif whosturn == 'Po':
                 if len(left) == 0:
@@ -82,11 +70,6 @@ while winner != 0:
                     o = o-1
                     if y not in left:
                         raise ValueError
-                # else:
-                # if (tac[o] == 'x') or (tac[o] == 'o'):
-                #     o = x = int(input("Try Again. O's turn to choose a square (1-9):"))
-                #     print("tac[y] = ", tac[y])
-                #     print("left[y] = ", left)
                     else:
                         tac.pop(o)
                         left.remove(y)
@@ -102,11 +85,12 @@ while winner != 0:
             else:
                 print("Game Over")
                 break
-            # print("\n",tac[0],"|",tac[1],"|",tac[2],"\n --+---+--\n",tac[3],"|",tac[4],"|",tac[5],"\n --+---+--\n",tac[6],"|",tac[7],"|",tac[8])
-
         except ValueError:
             y = 0
             print("That's not an option!")
     if len(left) == 0:
         print ("Cat's Game. No winner. Do you want to play again?")
     break
+
+if __name__ == "__main__":
+    main()
